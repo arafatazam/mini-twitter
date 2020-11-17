a24c66f289c65ad510d9c9574ad503c9fdf2d947
Md. Arafat Azam <arafatazam17@gmail.com>
2020-11-17 10:35:20 +0600

    Menu and page layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <div class="columns">
+      <aside class="menu column is-one-quarter">
+        <ul class="menu-list">
+          <li><router-link class="is-active" to="/">Home</router-link></li>
+          <li><router-link to="/me">Profile</router-link></li>
+        </ul>
+      </aside>
+      <router-view class="container column" />
+    </div>
   </div>
-  <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
